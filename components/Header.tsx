b64b030c1ecd1d9f283eb19c7c0304ef83a7c808
21bowler <samwachira224@gmail.com>
2025-06-07 19:48:39 +0300

    develop header and navlinks

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import { navLinks } from '../lib/data';
+import { Link, NavLink } from 'react-router';
 
 const Header = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <section className="flex justify-between items-center p-4">
+      <div>
+        <Link to="/">
+          <h1 className="font-bold text-2xl">DevLog</h1>
+        </Link>
+      </div>
+
+      <div>
+        <nav>
+          <div className="flex gap-4">
+            {navLinks.map(({ name, link }) => (
+              <NavLink
+                to={link}
+                key={name}
+                className={({ isActive }) =>
+                  isActive ? 'font-medium underline' : 'hover:text-gray-600'
+                }
+              >
+                {name}
+              </NavLink>
+            ))}
+          </div>
+        </nav>
+      </div>
+    </section>
+  );
 };
 
 export default Header;
