70fa87104cb76f9d4745187055a2d5a9af1aff51
21bowler <samwachira224@gmail.com>
2025-06-07 19:53:22 +0300

    modify with header and hero section

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,19 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import type { Route } from './+types/home';
+import Header from '../../components/Header';
+import HeroSection from 'components/HeroSection';
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: 'New React Router App' },
+    { name: 'description', content: 'Welcome to React Router!' },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <>
+      <Header />
+      <HeroSection />
+    </>
+  );
 }
