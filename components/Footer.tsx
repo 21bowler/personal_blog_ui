a35c6ba95271bb916703c078a4a2996e14d5359f
21bowler <samwachira224@gmail.com>
2025-06-12 22:45:37 +0300

    add footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer className="container flex justify-center py-10">
+      <p>
+        {' '}
+        Copyright &copy; {new Date().getFullYear()} The DevLog. All rights
+        reserved.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
