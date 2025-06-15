19dedd00d533f66810d71a56f82bf2d9bf73fa6e
21bowler <samwachira224@gmail.com>
2025-06-15 21:40:21 +0300

    add blog details

---
+++ app/routes/blog.tsx
@@ -0,0 +1,11 @@
+import BlogDetails from '../../components/BlogDetails';
+
+const Blog = () => {
+  return (
+    <div>
+      <BlogDetails />
+    </div>
+  );
+};
+
+export default Blog;
---
+++ components/BlogDetails.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const BlogDetails = () => {
+  return (
+    <section className="container">
+      {/*  Container */}
+      <div>
+        {/*  Header details meta data*/}
+        <div className="">
+          <h2 className="font-semibold text-2xl sm:text-4xl">
+            The Future of User Centered Design
+          </h2>
+          <p>
+            Exploring how empathy and usability are shaping the next generation
+            of design systems.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default BlogDetails;
