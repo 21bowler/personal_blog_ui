ef73391f54f3a60c6f8bc719d23195adeb091bf2
21bowler <samwachira224@gmail.com>
2025-06-28 16:23:36 +0300

    introduce call to action

---
+++ components/CTA.tsx
@@ -0,0 +1,26 @@
+const CallToAction = () => {
+  return (
+    <div className="flex container flex-col justify-between mb-4 bg-[#0A190F] text-white px-5 py-6 rounded-2xl sm:flex-row">
+      <div className="">
+        <h4 className="text-2xl sm:text-3xl sm:w-52 py-6">
+          Ready To Get Our New Stuff?
+        </h4>
+        <div className="relative">
+          <input
+            type="email"
+            placeholder="Your email"
+            className="bg-white text-black outline-none placeholder:text-gray-400 py-2 pl-2 pr-8 rounded-full"
+          />
+          <button className="absolute text-sm left-48 top-1 bg-[#0A190F]/90 rounded-full px-4 py-1.5 cursor-pointer transition-all hover:bg-[#0A190F]">
+            Send
+          </button>
+        </div>
+      </div>
+      <div className="sm:self-end pt-8 pb-4 text-sm sm:text-base">
+        <p>Devish blog for devs and enthusiasts.</p>
+      </div>
+    </div>
+  );
+};
+
+export default CallToAction;
