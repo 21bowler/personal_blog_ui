884fb6eb0ac84543e33bb131dc2c30b8685cb8c8
21bowler <samwachira224@gmail.com>
2025-06-28 16:22:24 +0300

    implement blog page with dummy data

--- app/routes/blog.tsx
+++ app/routes/blog.tsx
@@ -1,10 +1,29 @@
 import BlogDetails from '../../components/BlogDetails';
+import BlogHeroSection from '../../components/BlogHeroSection';
+import ArticleCard from '../../components/ArticleCard';
+import { articleResources } from '../../lib/data';
 
 const Blog = () => {
   return (
-    <div>
-      <BlogDetails />
-    </div>
+    <section className="container">
+      <div>
+        <BlogHeroSection />
+
+        {/*Blog cards below*/}
+        <div className="articles-grid mb-10 container">
+          {articleResources.map(({ title, description, tag, date, author }) => (
+            <ArticleCard
+              key={title}
+              title={title}
+              description={description}
+              tag={tag}
+              date={date}
+              author={author}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
   );
 };
 
