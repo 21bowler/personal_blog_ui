4e45b4ba9e065d27653627299e684210d3d266a8
21bowler <samwachira224@gmail.com>
2025-07-07 19:53:04 +0300

    add blogs and article creation route

--- app/routes.ts
+++ app/routes.ts
@@ -2,5 +2,6 @@ import { type RouteConfig, index, route } from '@react-router/dev/routes';
 
 export default [
   index('routes/home.tsx'),
-  route('/blogs', 'routes/blog.tsx'),
+  route('blogs', 'routes/blog.tsx'),
+  route('admin/create-article', 'routes/admin/create-article.tsx'),
 ] satisfies RouteConfig;
