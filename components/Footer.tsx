5d320d65a794300fb13fe69577d82d160ece40af
21bowler <samwachira224@gmail.com>
2025-07-09 21:51:58 +0300

    proper font and padding added

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="container flex justify-center py-10">
+    <footer className="container text-sm flex justify-center py-6 sm:py-10">
       <p>
         {' '}
         Copyright &copy; {new Date().getFullYear()} The DevLog. All rights
