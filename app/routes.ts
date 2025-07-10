ff526f8c3b0865e9eff58e680b1e347b8560a635
21bowler <samwachira224@gmail.com>
2025-07-10 14:46:27 +0300

    include routes for sign-up and sign-in

--- app/routes.ts
+++ app/routes.ts
@@ -4,4 +4,6 @@ export default [
   index('routes/home.tsx'),
   route('blogs', 'routes/blog.tsx'),
   route('admin/create-article', 'routes/admin/create-article.tsx'),
+  route('auth/sign-in', 'routes/auth/sign-in.tsx'),
+  route('auth/sign-up', 'routes/auth/sign-up.tsx'),
 ] satisfies RouteConfig;
