6d0dce5d8a6c45af6a48a7c9d8c614f4ea4cae98
21bowler <samwachira224@gmail.com>
2025-07-16 13:20:17 +0300

    feat: introduce user registration and sign in

---
+++ app/routes/auth/sign-in.tsx
@@ -0,0 +1,61 @@
+import AuthForm from '../../../components/AuthForm';
+import type { Route } from '../../../.react-router/types/app/routes/auth/+types/sign-in';
+import {
+  signInUser,
+  type SignInCredentials,
+} from '../../../services/auth-service';
+import { redirect, useActionData, useNavigation } from 'react-router';
+
+export const clientAction = async ({ request }: Route.ClientActionArgs) => {
+  const formData = await request.formData();
+  const email = formData.get('email') as string;
+  const password = formData.get('password') as string;
+
+  // Basic validation
+  if (!email || !password) {
+    return {
+      error: 'Please enter a valid email and password',
+    };
+  }
+
+  try {
+    const credentials: SignInCredentials = { email, password };
+
+    const { user, session } = await signInUser(credentials);
+
+    if (user && session) {
+      // IMPORTANT (NOTE TO FUTURE SAM): session is stored in localStorage by Supabase client,
+      return redirect('/');
+    } else {
+      return {
+        error:
+          'Sign-in process completed, but no user data returned. Check Supabase settings.',
+      };
+    }
+  } catch (error: any) {
+    console.error('Sign-in action error: ', error.message);
+
+    // return to the UI
+    return {
+      error: error.message || 'An unexpected error occurred during sign-in.',
+    };
+  }
+};
+
+const SignIn = () => {
+  const actionData = useActionData();
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === 'submitting';
+
+  return (
+    <AuthForm
+      subtitleText="Sign in to your account"
+      togglePromptText="Dont have an account?"
+      buttonText={isSubmitting ? 'Signing in...' : 'Sign In'}
+      toggleLink="/auth/sign-up"
+      actionData={actionData}
+      isSubmitting={isSubmitting}
+    />
+  );
+};
+export default SignIn;
---
+++ app/routes/auth/sign-up.tsx
@@ -0,0 +1,59 @@
+import { redirect, useActionData, useNavigation } from 'react-router';
+import AuthForm from '../../../components/AuthForm';
+import { signUp, type SignUpCredentials } from '../../../services/auth-service';
+import type { Route } from './+types/sign-up';
+
+export async function clientAction({ request }: Route.ClientActionArgs) {
+  const formData = await request.formData();
+  const email = formData.get('email') as string;
+  const password = formData.get('password') as string;
+
+  //Basic validation
+  if (!email || !password) {
+    return {
+      error: 'Please enter a valid email and password',
+    };
+  }
+
+  try {
+    const credentials: SignUpCredentials = { email, password };
+
+    // call supabase sign-up function
+    const { user } = await signUp(credentials);
+
+    if (user) {
+      // Needs a toaster here for signing up
+      return redirect('/');
+    } else {
+      return {
+        error:
+          'Sign-up process completed, but no user data returned. Check Supabase settings.',
+      };
+    }
+  } catch (error: any) {
+    console.error('Sign-up action error: ', error);
+
+    // return to the UI
+    return {
+      error: error.message || 'An unexpected error occurred during sign-up.',
+    };
+  }
+}
+
+const SignUp = () => {
+  const navigation = useNavigation();
+  const actionData = useActionData();
+  const isSubmitting = navigation.state === 'submitting';
+
+  return (
+    <AuthForm
+      subtitleText="Create a new account"
+      togglePromptText="Already have an account?"
+      buttonText={isSubmitting ? 'Signing up...' : 'Sign Up'}
+      toggleLink="/auth/sign-in"
+      actionData={actionData}
+      isSubmitting={isSubmitting}
+    />
+  );
+};
+export default SignUp;
