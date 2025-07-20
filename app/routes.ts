7c08fd66c1b83845d7655a5296bba49cbeee9b00
21bowler <samwachira224@gmail.com>
2025-07-20 09:39:57 +0300

    new: add nested routes for admin

--- app/routes.ts
+++ app/routes.ts
@@ -3,7 +3,15 @@ import { type RouteConfig, index, route } from '@react-router/dev/routes';
 export default [
   index('routes/home.tsx'),
   route('blogs', 'routes/blog.tsx'),
-  route('admin/create-article', 'routes/admin/create-article.tsx'),
+  route('article/:slug', 'routes/blog-details.tsx'),
+
+  // Nested admin routes
+  route('admin', 'routes/admin/dashboard.tsx', [
+    route('all-articles', 'routes/admin/admin-articles.tsx'),
+    route('create-article', 'routes/admin/create-article.tsx'),
+    route('edit-article/:id', 'routes/admin/edit-article.tsx'),
+  ]),
+
   route('auth/sign-in', 'routes/auth/sign-in.tsx'),
   route('auth/sign-up', 'routes/auth/sign-up.tsx'),
 ] satisfies RouteConfig;
