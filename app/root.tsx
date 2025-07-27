c0318617d6276deba20016a0cab9a1522bbf7b0c
21bowler <samwachira224@gmail.com>
2025-07-27 14:54:32 +0300

    add react imp

--- app/root.tsx
+++ app/root.tsx
@@ -7,6 +7,7 @@ import {
   ScrollRestoration,
 } from 'react-router';
 
+import React from 'react';
 import type { Route } from './+types/root';
 import './app.css';
 import Header from '../components/Header';
