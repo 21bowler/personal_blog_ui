12bdfab70d0dd1717202e4140334c2c7772892d7
21bowler <samwachira224@gmail.com>
2025-07-28 19:58:50 +0300

    dynamically added data to the meta

--- components/AuthorMeta.tsx
+++ components/AuthorMeta.tsx
@@ -5,18 +5,26 @@ import {
   Square3Stack3DIcon,
 } from '@heroicons/react/24/outline';
 
-const AuthorMeta = ({ justify }: { justify: string }) => {
+const AuthorMeta = ({
+  justify,
+  author,
+  tag,
+}: {
+  justify: string;
+  author: string;
+  tag: string;
+}) => {
   return (
     <article className={`flex  text-sm gap-4 items-center ${justify} mt-4`}>
-      <span className="flex-center-gap-1">
+      <span className="flex items-center gap-1">
         <PencilIcon className="size-5 stroke-black" />
-        <span className="text-gray-600">Stephen Chao</span>
+        <span className="text-gray-600">{author}</span>
       </span>
-      <span className="flex-center-gap-1">
+      <span className="flex items-center gap-1">
         <Square3Stack3DIcon className="size-5 stroke-black" />
-        <span className="text-gray-600">Design</span>
+        <span className="text-gray-600">{tag}</span>
       </span>
-      <span className="flex-center-gap-1">
+      <span className="flex items-center gap-1">
         <ClockIcon className="size-5 stroke-black" />
         <span className="text-gray-600">12 min. read</span>
       </span>
