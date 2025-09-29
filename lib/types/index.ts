8fcfeea2f418db24f9798caa8ea073773c244149
21bowler <samwachira224@gmail.com>
2025-09-29 16:14:20 +0300

    create a centralized home for major types

---
+++ lib/types/index.ts
@@ -0,0 +1,12 @@
+export interface Article {
+  id: number;
+  title: string;
+  tag: string;
+  description: string;
+  author: string;
+  image_url: string;
+  published: boolean;
+  content: string;
+  slug: string;
+  created_at: string;
+}
